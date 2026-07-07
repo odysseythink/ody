d4a65b9076ecc380da4e175f1c911efd88a70e77
ranwei693532 <ranwei693532@163.com>
2026-07-08 01:09:41 +0800

    feat(collaboration-mode-templates): expose PLAN_RIGOR_COVERAGE constant

--- collaboration-mode-templates/src/lib.rs
+++ collaboration-mode-templates/src/lib.rs
@@ -2,3 +2,4 @@ pub const PLAN: &str = include_str!("../templates/plan.md");
 pub const DEFAULT: &str = include_str!("../templates/default.md");
 pub const EXECUTE: &str = include_str!("../templates/execute.md");
 pub const PAIR_PROGRAMMING: &str = include_str!("../templates/pair_programming.md");
+pub const PLAN_RIGOR_COVERAGE: &str = include_str!("../templates/plan_rigor_coverage.md");
